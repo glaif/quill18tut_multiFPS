60925651b2edc9c748d7faeffa659c8346401ed7
steve <smaldone@gmail.com>
2017-08-03 16:42:53 -0400

    Started to plumb in photonnetwork stuff

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour {
+
+    public Camera standbyCamera;
+
+	// Use this for initialization
+	void Start () {
+        Connect();
+	}
+
+    void Connect() {
+        PhotonNetwork.offlineMode = true;
+        PhotonNetwork.ConnectUsingSettings("dev_001");
+    }
+
+    void OnGUI() {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby() {
+        Debug.Log("OnJoinedLobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnRandomJoinFailed() {
+        Debug.Log("OnRandomJoinFailed");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom() {
+        Debug.Log("OnJoinedRoom");
+        SpawnMyPlayer();
+    }
+
+    void SpawnMyPlayer() {
+        PhotonNetwork.Instantiate("PlayerController", Vector3.zero, Quaternion.identity, 0);
+        standbyCamera.enabled = false;
+    }
+}
