2c682e32c35f686b7fc58651d91baffe43cdfea9
steve <smaldone@gmail.com>
2017-08-17 17:42:19 -0400

    Point in time commit for UI

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -8,23 +8,37 @@ public class NetworkManager : MonoBehaviour {
     public GameObject standbyCamera;
     SpawnSpot[] spawnSpots;
 
+    bool connecting = false;
+
     // Use this for initialization
     void Start () {
         spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
-        Connect();
 	}
 
     void Connect() {
-        if (offlineMode) {
-            PhotonNetwork.offlineMode = true;
-            OnJoinedLobby();
-            return;
-        }
         PhotonNetwork.ConnectUsingSettings("dev_001");
     }
 
     void OnGUI() {
         GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+
+        if (PhotonNetwork.connected == false && connecting == false) {
+            GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Single Player")) {
+                PhotonNetwork.offlineMode = true;
+                connecting = true;
+                OnJoinedLobby();
+            }
+            if (GUILayout.Button("Multi-Player")) {
+                connecting = true;
+                Connect();
+            }
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+            GUILayout.EndArea();
+        }
     }
 
     // Bypassed if OFFLINE == true
@@ -54,10 +68,7 @@ public class NetworkManager : MonoBehaviour {
         GameObject myPlayerGO = PhotonNetwork.Instantiate("PlayerController", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
         standbyCamera.SetActive(false);
 
-        // NOTE: Must enable camera *before* controller, otherwise controller will have 
-        // a null reference to the camera and will not get a new one
         myPlayerGO.transform.Find("FirstPersonCharacter").gameObject.SetActive(true);
-        //((MonoBehaviour)myPlayerGO.GetComponent("FirstPersonController")).enabled = true;
         ((MonoBehaviour)myPlayerGO.GetComponent("MouseLook")).enabled = true;
         ((MonoBehaviour)myPlayerGO.GetComponent("PlayerMovement")).enabled = true;
         ((MonoBehaviour)myPlayerGO.GetComponent("PlayerShooting")).enabled = true;
